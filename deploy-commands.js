d25110cfebc11a1e6c788209af9b8defd61a891d
Carter Clark <crtrclark@gmail.com>
2022-05-25 22:39:30 -0500

    fixed vulnerability and changed deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,7 +7,7 @@ const { Routes } = require("discord-api-types/v9");
 
 const commands = [
   new SlashCommandBuilder()
-    .setName("ping")
+    .setName("testing")
     .setDescription("Replies with pong!"),
   new SlashCommandBuilder()
     .setName("take_roles")
@@ -18,12 +18,6 @@ const commands = [
   new SlashCommandBuilder()
     .setName("list_roles")
     .setDescription("Replies with a list of roles in the server"),
-  new SlashCommandBuilder()
-    .setName("enable_assign_roles")
-    .setDescription("Turns auto role assignemnt on"),
-  new SlashCommandBuilder()
-    .setName("disable_assign_roles")
-    .setDescription("Turns auto role assignemnt off"),
   new SlashCommandBuilder()
     .setName("check_assign_roles")
     .setDescription("Checks if role assignment is enabled"),
