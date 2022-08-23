4b5aaed2dda9ecd5e5a6f5221c3b04ce57ec67ac
Isiah Taylor <it44822@gmail.com>
2022-08-23 16:25:53 -0500

    Revert "npm installs and changing the deploy commands"
    This reverts commit a741ec7327970e285d4558fb0f5830b78038e5ac.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,53 +1,36 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const dotenv = require("dotenv");
+dotenv.config();
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'actions');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
-}
-
-
-
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const { SlashCommandBuilder } = require("@discordjs/builders");
-// const { REST } = require("@discordjs/rest");
-// const { Routes } = require("discord-api-types/v9");
-
-// const commands = [
-//   new SlashCommandBuilder()
-//     .setName("role_me")
-//     .setDescription("Roles the user based upon input")
-//     .addStringOption((option) =>
-//       option.setName("name").setDescription("Enter your name").setRequired(true)
-//     )
-//     .addStringOption((option) =>
-//       option
-//         .setName("classes")
-//         .setDescription("Enter your classes")
-//         .setRequired(true)
-//     ),
-//   new SlashCommandBuilder()
-//     .setName("info")
-//     .setDescription("Gives info about server"),
-//   new SlashCommandBuilder()
-//     .setName("list_roles")
-//     .setDescription("Replies with a sorted list of roles in the server"),
-//   new SlashCommandBuilder()
-//     .setName("take_roles")
-//     .setDescription("takes all class roles from everyone")
-//     .addStringOption((option) =>
-//       option.setName("yes_or_no").setDescription("confirm").setRequired(true)
-//     ),
-// ].map((command) => command.toJSON());
+const commands = [
+  new SlashCommandBuilder()
+    .setName("role_me")
+    .setDescription("Roles the user based upon input")
+    .addStringOption((option) =>
+      option.setName("name").setDescription("Enter your name").setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("classes")
+        .setDescription("Enter your classes")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Gives info about server"),
+  new SlashCommandBuilder()
+    .setName("list_roles")
+    .setDescription("Replies with a sorted list of roles in the server"),
+  new SlashCommandBuilder()
+    .setName("take_roles")
+    .setDescription("takes all class roles from everyone")
+    .addStringOption((option) =>
+      option.setName("yes_or_no").setDescription("confirm").setRequired(true)
+    ),
+].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(process.env.BOT_AUTH_TOKEN);
 
