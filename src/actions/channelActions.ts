49ff7bacb4c0ace913ef56c8946b69812ea03488
Carter Clark <crtrclark@gmail.com>
2022-08-22 22:29:08 -0500

    slight change

--- src/actions/channelActions.ts
+++ src/actions/channelActions.ts
@@ -58,7 +58,7 @@ export async function secretChannelResponses(
     case `info`: {
       updateUnchangableNameMemberList(client, unchangableNameMemberList);
       await interaction.reply(
-        `Server name: ${interaction!.guild!.name}\nServer id: ${
+        `Server name: ${interaction.guild!.name}\nServer id: ${
           interaction!.guild!.id
         }\n` +
           `classPrefixList: [${classPrefixList}]\n` +
